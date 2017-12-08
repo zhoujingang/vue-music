1289e96bbec5304674e7f97450e4a4efd57e2a78
zhoujingang <1253822097@qq.com>
2017-12-08 18:44:05 +0800

    2017-7-7

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -5,10 +5,47 @@
 </template>
 
 <script>
+  import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+
   export default {
     data() {
       return {
-
+        song: []
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'singer'
+      ])
+    },
+    created() {
+      this._getDetail()
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list)
+            console.log(this.songs)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
       }
     }
   }
