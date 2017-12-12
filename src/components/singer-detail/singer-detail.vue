31d7e882984c32162643eecafa674a9935cf7f5b
zhoujingang <1253822097@qq.com>
2017-12-12 18:54:43 +0800

    2017-12-12

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slider">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage"  :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -9,14 +9,24 @@
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
     data() {
       return {
-        song: []
+        songs: []
       }
     },
+    components: {
+      MusicList
+    },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
