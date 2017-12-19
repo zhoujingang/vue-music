e2e3b376e610a60b5b8dec50083c5a3a9d77138b
zhoujingang <1253822097@qq.com>
2017-12-19 18:16:38 +0800

    2017-12-19

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,19 +1,114 @@
 <template>
-    <div>
-        3
+    <div class="search">
+      <div class="search-box-wrapper">
+        <search-box ref="searchBox" @query="onQueryChange"></search-box>
+      </div>
+      <div class="shortcut-wrapper">
+        <div class="shortcut">
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li @click="addQuery(item.k)" class="item" v-for="item in hotKey">
+                <span>{{item.k}}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="search-result">
+        <suggest :query="query"></suggest>
+      </div>
     </div>
 </template>
 
 <script>
-    export default {
-      data () {
-        return {
+  import SearchBox from 'base/search-box/search-box'
+  import {getHotKey} from 'api/search'
+  import {ERR_OK} from 'api/config'
+  import Suggest from 'components/suggest/suggest'
 
-        }
+  export default {
+    data () {
+      return {
+        hotKey: [],
+        query: ''
       }
+    },
+    created() {
+      this._getHotKey()
+    },
+    methods: {
+      addQuery(query) {
+        // console.log(e)
+        this.$refs.searchBox.setQuery(query)
+      },
+      onQueryChange(query) {
+        // console.log(query)
+        this.query = 'query'
+      },
+      _getHotKey() {
+        getHotKey().then((res) => {
+          if (res.code === ERR_OK) {
+            this.hotKey = res.data.hotkey.slice(0, 10)
+          }
+        })
+      }
+    },
+    components: {
+      SearchBox,
+      Suggest
     }
+  }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
 
+  .search
+    .search-box-wrapper
+      margin: 20px
+    .shortcut-wrapper
+      position: fixed
+      top: 178px
+      bottom: 0
+      width: 100%
+      .shortcut
+        height: 100%
+        overflow: hidden
+        .hot-key
+          margin: 0 20px 20px 20px
+          .title
+            margin-bottom: 20px
+            font-size: $font-size-medium
+            color: $color-text-l
+          .item
+            display: inline-block
+            padding: 5px 10px
+            margin: 0 20px 10px 0
+            border-radius: 6px
+            background: $color-highlight-background
+            font-size: $font-size-medium
+            color: $color-text-d
+        .search-history
+          position: relative
+          margin: 0 20px
+          .title
+            display: flex
+            align-items: center
+            height: 40px
+            font-size: $font-size-medium
+            color: $color-text-l
+            .text
+              flex: 1
+            .clear
+              extend-click()
+              .icon-clear
+                font-size: $font-size-medium
+                color: $color-text-d
+    .search-result
+      position: fixed
+      width: 100%
+      top: 178px
+      bottom: 0
 </style>
